d5ba9933ab6ad12f744dec36adad5610eca7f986
Lars Bilke <lars.bilke@ufz.de>
2017-01-03 14:19:45 +0100

    Fixed additional qt builds.

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bilke")
     builder.add_common_builds(pure_c=False)
-    builder.add({}, {"VTK:qt": True})
+    builder.add({"arch": "x86_64", "build_type": "Release"}, {"VTK:qt": True})
+    builder.add({"arch": "x86_64", "build_type": "Debug"}, {"VTK:qt": True})
     builder.run()
