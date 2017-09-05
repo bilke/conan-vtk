1bccaa5ab9e63ed9c58c1b11eaa3eac4ddb5a235
Lars Bilke <lars.bilke@ufz.de>
2017-09-05 10:13:01 +0200

    Fixed upload repo.

--- build.py
+++ build.py
@@ -1,13 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(
-        username = "bilke",
-        upload = "https://ogs.jfrog.io/ogs/api/conan/conan",
-        archs = ["x86_64"]
-    )
+    builder = ConanMultiPackager(archs = ["x86_64"])
     builder.add_common_builds(pure_c=False)
-    #builder.add({"arch": "x86_64", "build_type": "Release"}, {"VTK:mpi": True})
-    # builder.add({"arch": "x86_64", "build_type": "Release"}, {"VTK:qt": True})
-    # builder.add({"arch": "x86_64", "build_type": "Debug"}, {"VTK:qt": True})
     builder.run()
