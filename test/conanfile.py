7e05b2dd941544ba01bc296885a77ae17d7cbc98
Lars Bilke <lars.bilke@ufz.de>
2018-04-17 12:55:52 +0200

    Fixed test package CMake.

--- test/conanfile.py
+++ test/conanfile.py
@@ -11,8 +11,8 @@ class VTKReuseConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake . %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
