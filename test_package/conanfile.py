cfe462c86e4392bc17cf47cf5637fe9f065781a2
Lars Bilke <lars.bilke@ufz.de>
2018-06-28 22:36:23 +0200

    Refactor test.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,25 +1,24 @@
-from conans.model.conan_file import ConanFile
-from conans import CMake
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
-channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "bilke")
+class TestPackageConan(ConanFile):
+  settings = "os", "compiler", "build_type", "arch"
+  generators = "cmake"
 
-class DefaultNameConan(ConanFile):
-    name = "DefaultName"
-    version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    requires = "VTK/8.1.0@%s/%s" % (username, channel)
+  def build(self):
+    cmake = CMake(self)
+    cmake.configure()
+    cmake.build()
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin")
-        self.copy(pattern="*.dylib", dst="bin", src="lib")
-
-    def test(self):
-        self.run("cd bin && .%smyvtk" % os.sep)
+  def test(self):
+    with tools.environment_append(RunEnvironment(self).vars):
+      bin_path = os.path.join("bin", "test_package")
+      if self.settings.os == "Windows":
+        self.run(bin_path)
+      elif self.settings.os == "Macos":
+        self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+      else:
+        self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
