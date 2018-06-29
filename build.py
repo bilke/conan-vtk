02549a8f984caa16cdfa543a32f809b46dacaa5e
Lars Bilke <lars.bilke@ufz.de>
2018-06-29 09:25:27 +0200

    fPIC is handled automatically.

--- build.py
+++ build.py
@@ -5,21 +5,14 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs = ["x86_64"])
     builder.add_common_builds(pure_c=False)
-    filtered_builds = []
-    if platform.system() == "Linux":
-        for settings, options, env_vars, build_requires in builder.builds:
-            filtered_builds.append([settings, options])
-            new_options = copy.copy(options)
-            new_options["VTK:fPIC"] = True
-            filtered_builds.append([settings, new_options, env_vars, build_requires])
-        builder.builds = filtered_builds
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["compiler"] == "Visual Studio":
-            if settings["compiler.runtime"] == "MT" or settings["compiler.runtime"] == "MTd":
+    items = []
+    for item in builder.items:
+        if item.settings["compiler"] == "Visual Studio":
+            if item.settings["compiler.runtime"] == "MT" or item.settings["compiler.runtime"] == "MTd":
                 # Ignore MT runtime
                 continue
-        new_options = copy.copy(options)
+        new_options = copy.copy(item.options)
         new_options["VTK:qt"] = True
-        filtered_builds.append([settings, new_options, env_vars, build_requires])
-    builder.builds = filtered_builds
+        items.append([item.settings, new_options, item.env_vars, item.build_requires])
+    builder.items = items
     builder.run()
