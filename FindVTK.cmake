52c5e75329dff9c8d5609365ecf9787e3eee6ddd
Lars Bilke <lars.bilke@ufz.de>
2019-07-12 13:16:08 +0200

    Fix in FindVTK

--- FindVTK.cmake
+++ FindVTK.cmake
@@ -1,5 +1,5 @@
 set(VTK_DIR ${CONAN_VTK_ROOT})
-include(${CONAN_VTK_ROOT}/lib/cmake/vtk-8.1/VTKConfig.cmake)
+include(${CONAN_VTK_ROOT}/lib/cmake/vtk-8.2/VTKConfig.cmake)
 
 mark_as_advanced(
 	VTK_DIR
